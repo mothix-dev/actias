2efd5b6599974f96e35fa79287ae5bd5437d0512
velleda <pugduddly@gmail.com>
2022-05-03 16:28:00 -0500

    more stuff

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 
 echo "(ctrl+c to exit)"
 
-qemu-system-i386 -machine type=pc-i440fx-3.1 -kernel target/i586-unknown-none/release/ockernel -display none -serial stdio
+#qemu-system-i386 -machine type=pc-i440fx-3.1 -kernel target/i586-unknown-none/release/ockernel -display none -serial stdio
+qemu-system-i386 -machine type=pc-i440fx-3.1 -kernel target/i586-unknown-none/release/ockernel -serial stdio
